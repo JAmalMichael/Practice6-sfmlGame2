6deecc23a6a7c8be6dd9119a61a1b2065cce46cf
Igbanesi michael <eshimi090@gmail.com>
2025-10-15 22:19:32 +0100

    Component Shape header file.

---
+++ Practice6-sfmlGame2/CShape.h
@@ -0,0 +1,24 @@
+#ifndef CSHAPE_H
+#define CSHAPE_H
+
+#include "Component.h"
+#include <SFML/Graphics.hpp>
+
+
+class Cshape : public Component 
+{
+	sf::CircleShape circle;
+	Cshape( float radius, int points, const sf::Color & fill, const sf::Color outline, float thickness)
+	{
+		circle.setRadius(radius);
+		circle.setPointCount(points);
+		circle.setFillColor(fill);
+		circle.setOutlineColor(outline);
+		circle.setOutlineThickness(thickness);
+		circle.setOrigin(radius, radius);
+	}
+};
+
+
+
+#endif
