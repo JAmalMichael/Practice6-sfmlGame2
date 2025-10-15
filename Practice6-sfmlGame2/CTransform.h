235a96b76cd8f02445ade362c75c7de8cb45e948
Igbanesi michael <eshimi090@gmail.com>
2025-10-15 16:06:44 +0100

    Component Transform header

--- Practice6-sfmlGame2/CTransform.h
+++ Practice6-sfmlGame2/CTransform.h
@@ -2,7 +2,17 @@
 #define CTRANSFORM_H
 
 #include "Component.h"
+#include "Vec2.h"
 
+struct CTransform : public Component
+{
+	Vec2 position;
+	Vec2 velocity;
+	float angle = 0.0f;
+
+	CTransform(const Vec2& pos = {}, const Vec2 vel = {}, float ang = 0.f)
+		:position(pos), velocity(vel), angle(ang) {	};
+};
 
 
 
